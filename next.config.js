32aaea2cd0410959b6ab15a70f037cd9dc719e32
Lucas <lucas.simon.b@gmail.com>
2019-10-19 01:31:03 -0300

    Add automatic sass variables import config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,28 @@
 const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS(
-  withSass({
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000
-          }
-        }
-      });
+const resourcesLoader = {
+  loader: 'sass-resources-loader',
+  options: {
+    resources: ['./themes/_colors.scss']
+  }
+};
 
-      return config;
-    }
-  })
-);
+module.exports = withSass({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: '[local]___[hash:base64:5]'
+  },
+  webpack(config) {
+    // eslint-disable-next-line array-callback-return
+    config.module.rules.map(rule => {
+      if (
+        rule.test.source.includes('scss') ||
+        rule.test.source.includes('sass')
+      ) {
+        rule.use.push(resourcesLoader);
+      }
+    });
+    return config;
+  }
+});
